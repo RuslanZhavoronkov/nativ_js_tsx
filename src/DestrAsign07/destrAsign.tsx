198411303d213198fae0f2a1c37f421528fd9993
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-07-06 21:33:14 +0300

    Lesson 07 -  destruction object

---
+++ src/DestrAsign07/destrAsign.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+type TitleType = {
+    title: number
+}
+
+type StreetType = {
+    title:string
+}
+
+type AddressType = {
+    street:StreetType
+}
+export type ManType = {
+    name:string,
+    age: number,
+    lessons: Array<TitleType>,
+    address: AddressType
+   
+}
+
+type  PropsType = {
+    title: string
+    man: ManType
+    food: Array <string>
+    car: {model: string}
+}
+
+
+export const  ManComponent: React.FC<PropsType> = ({props}) => {
+
+   const {title, man, ...restProps} = props
+
+    return <div>
+        <h1>{title}</h1>
+        <hr/>
+        <div>
+            {restProps.car.model}
+        </div>
+        <div>
+            {man.name}
+        </div>
+    </div>
+}
